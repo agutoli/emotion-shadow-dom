db99008df10efae6adcd508d1d95de98aef5c5db
Bruno Agutoli <bruno.agutoli@gmail.com>
2017-08-21 18:52:44 +0530

    updated version

--- src/index.js
+++ src/index.js
@@ -5,25 +5,35 @@ import ShadowDOM from 'react-shadow'
 export default (element) => {
   class EmotionShadowDOMWrapper extends React.Component {
     render() {
-      return React.createElement(element, {...this.props, ref: this.emotionInject});
+      return React.createElement(element, {...this.props, innerRef: this.emotionInject});
     }
 
     emotionInject = (node) => {
       const { injectStyle } = this.props;
+      node = node || ReactDOM.findDOMNode(this);
+
+      node.classList && injectStyle(node.classList);
+
       const didUpdate = element.prototype.componentDidUpdate || function() {};
+
       element.prototype.componentDidUpdate = () => {
-        node.className && injectStyle(node.className);
+        node.className && injectStyle(node.classList);
         didUpdate();
       };
+    }
+
+    componentDidMount() {
+      // this.emotionInject();
     }
   }
 
   return class ShadowDOMHelper extends React.PureComponent {
     injectStyle = (prefixId) => {
       const cssStyleSheet = Array.from(document.querySelectorAll('[data-emotion]')).slice(-1)[0];
+      const className = Array.from(prefixId).slice(-1)[0];
       if (cssStyleSheet) {
         Array.from(cssStyleSheet.sheet.cssRules).forEach((cssRule) => {
-          if ((new RegExp(prefixId)).test(cssRule.selectorText)) {
+          if ((new RegExp(className)).test(cssRule.selectorText.replace(/^\./, ''))) {
             this.styleTag.type = 'text/css';
             if (this.styleTag.styleSheet){
               this.styleTag.styleSheet.cssText = cssRule.cssText;
